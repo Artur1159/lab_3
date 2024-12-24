830afbd17cc3c945159775e3aa24a2749648675e
Artur1159 <arturgetz11@gmail.com>
2024-12-24 15:46:47 +0300

    Gitignore

--- src/TableExample.java
+++ src/TableExample.java
@@ -1,15 +1,110 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class TableExample {
+    public static void main(String[] args) {
+        // Создаем главное окно
+        JFrame frame = new JFrame("Приложение");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+
+        // Добавляем меню
+        JMenuBar menuBar = new JMenuBar();
+        JMenu helpMenu = new JMenu("Справка");
+
+        // Пункт "О программе"
+        JMenuItem aboutItem = new JMenuItem("О программе");
+        aboutItem.addActionListener(e -> {
+            // Показываем диалог "О программе"
+            JOptionPane.showMessageDialog(
+                    frame, // Передаем окно как родителя
+                    "Автор: Гец Артур\nГруппа: 7",
+                    "О программе",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+        });
+        helpMenu.add(aboutItem);
+        menuBar.add(helpMenu);
+        frame.setJMenuBar(menuBar);
+
+        // Таблица
+        Object[][] data = {
+                {3.0, "x^2 + 2x + 1", null},
+                {5.5, "2x^3 + x + 1", null},
+                {2.0, "x^2 - 4", null},
+                {4.3, "3x + 2", null}
+        };
+
+        // Вычисляем значения для третьего столбца
+        for (Object[] row : data) {
+            double value = (double) row[0];
+            row[2] = (value % 1 == 0); // true, если дробной части нет
+        }
+
+        // Создаем таблицу
+        String[] columnNames = {"Значение", "Многочлен", "Точное значение?"};
+        PolynomialTableModel model = new PolynomialTableModel(data, columnNames);
+        JTable table = new JTable(model);
+
+        // Устанавливаем отображение флажков в третьем столбце
+        table.getColumnModel().getColumn(2).setCellRenderer(table.getDefaultRenderer(Boolean.class));
+        table.getColumnModel().getColumn(2).setCellEditor(table.getDefaultEditor(Boolean.class));
+
+        // Добавляем таблицу в окно
+        frame.add(new JScrollPane(table), BorderLayout.CENTER);
+
+        // Отображаем окно
+        frame.setVisible(true);
+    }
+
+    // Класс модели таблицы
+    static class PolynomialTableModel extends AbstractTableModel {
+        private final Object[][] data;
+        private final String[] columnNames;
+
+        public PolynomialTableModel(Object[][] data, String[] columnNames) {
+            this.data = data;
+            this.columnNames = columnNames;
+        }
+
+        @Override
+        public int getRowCount() {
+            return data.length;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            return columnNames[column];
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return data[rowIndex][columnIndex];
+        }
+
+        @Override
+        public void setValueAt(Object value, int rowIndex, int columnIndex) {
+            data[rowIndex][columnIndex] = value;
+            fireTableCellUpdated(rowIndex, columnIndex);
+        }
+
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return columnIndex == 2; // Только третий столбец редактируем
+        }
+
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            if (columnIndex == 2) {
+                return Boolean.class; // Третий столбец содержит булевские значения
+            }
+            return String.class; // Остальные столбцы строки или числа
         }
     }
 }
